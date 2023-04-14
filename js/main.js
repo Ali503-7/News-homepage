1f54eb61fe031aac326f116528d3367657596532
Ali503-7 <alimoali354@gmail.com>
2023-04-14 19:44:59 +0200

    fix the footer seciton scroll behavier

--- js/main.js
+++ js/main.js
@@ -1,45 +1,57 @@
-let Mmenu = document.querySelector(".mobile-I-links")
-let threeLins = document.querySelector(".lins-mobile")
-let head = document.querySelector('header')
-let main = document.querySelector('main')
-let footer = document.querySelector('.bottom')
-let ax = document.querySelector("body .mobile-I-links div")
+const Mmenu = document.querySelector(".mobile-I-links");
+const threeLins = document.querySelector(".lins-mobile");
+const head = document.querySelector("header");
+const main = document.querySelector("main");
+const footer = document.querySelector(".bottom");
+const ax = document.querySelector("body .mobile-I-links div");
 
-threeLins.onclick = () => { 
-    Mmenu.style.right = 0
-    head.style.opacity = "0.5"
-    main.style.opacity = "0.5"
-    footer.style.opacity = "0.5"
-  Mmenu.style.display = "inline"
-  Mmenu.style.position = "fixed"
-}
+threeLins.onclick = () => {
+  Mmenu.style.right = 0;
+  head.style.opacity = "0.5";
+  main.style.opacity = "0.5";
+  footer.style.opacity = "0.5";
+  Mmenu.style.display = "inline";
+  Mmenu.style.position = "fixed";
+};
 
 ax.onclick = () => {
-  Mmenu.style.right = "-500px"
-  head.style.opacity = "1"
-      main.style.opacity = "1"
-    footer.style.opacity = "1"
-  Mmenu.style.display = "none"
-}
+  Mmenu.style.right = "-500px";
+  head.style.opacity = "1";
+  main.style.opacity = "1";
+  footer.style.opacity = "1";
+  Mmenu.style.display = "none";
+};
 
 // animation on scroll
 
-let b_one = document.querySelector("#one");
-let b_two = document.querySelector("#two");
-let b_three = document.querySelector("#three");
+const b_one = document.querySelector(".one");
+const b_two = document.querySelector(".two");
+const b_three = document.querySelector(".three");
 
+// to make the start animations only work when the document is loaded
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.querySelector("header");
+  const mainP = document.querySelector("main .future p");
+  const mainRead = document.querySelector("main .future .read");
+  const mainImg = document.querySelector("main .main-img");
+  const mainSide = document.querySelector("main .right");
+  const whenReady = (...divs) => {
+    divs.forEach((div) => {
+      div.classList.add("animate");
+    });
+  }; /* add the 'animate' class to start the animation */
+  setTimeout(() => {
+    whenReady(header, mainP, mainRead, mainImg, mainSide);
+  }, 500);
 
-
-window.onscroll = () => {
-  if (window.scrollY >= footer.offsetTop - 700) {
-    b_one.style.animationName = "F-right"
-    b_one.style.display = "grid"
-    b_two.style.animationName = "F-bottom"
-    b_two.style.display = "grid"
-    b_two.style.animationDuration = "1.5s"
-    b_three.style.animationName = "F-left"
-    b_three.style.display = "grid"
-    b_three.style.animationDuration = "1.7s"
-  };
-};
-
+  // update event listener to trigger on touch devices
+  window.addEventListener(
+    "scroll",
+    () => {
+      if (window.scrollY >= footer.offsetTop - 700) {
+        whenReady(b_one, b_two, b_three);
+      }
+    },
+    { passive: true }
+  );
+});
